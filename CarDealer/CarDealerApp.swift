12fa8613570ba905bf301451639a86846c3f5eb3
Katellyn Hyker <llyn00@yahoo.com>
2025-09-11 10:56:06 -0400

    Initial Commit

---
+++ CarDealer/CarDealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDealerApp.swift
+//  CarDealer
+//
+//  Created by Student on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CarDealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
