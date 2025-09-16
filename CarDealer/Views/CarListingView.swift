d8f68f3505639845283afbae1a04f63a071b43f0
Katellyn Hyker <llyn00@yahoo.com>
2025-09-16 14:19:58 -0400

    Created very basic CarListingView

--- CarDealer/Views/CarListingView.swift
+++ CarDealer/Views/CarListingView.swift
@@ -5,15 +5,34 @@
 //  Created by Katellyn Hyker on 9/11/25.
 //
 
-import Foundation
 import SwiftUI
 
 struct CarListingView: View {
+    let listing: Listing?
+    let car: Car?
+    
+    init(listing: Listing) {
+        self.listing = listing
+        self.car = nil
+    }
+    
+    init(car: Car) {
+        self.listing = nil
+        self.car = car
+    }
+    
+    
     var body: some View {
-        Text("Car Listing")
+        if let listing = listing {
+            Text("Listing: \(listing.car.brand) \(listing.car.model) \(listing.car.year)")
+        } else if let car = car {
+            Text("Car: \(car.brand) \(car.model) \(car.year)")
+        } else {
+            Text("No data")
+        }
     }
 }
 
 #Preview {
-    CarListingView()
+    //CarListingView()
 }
