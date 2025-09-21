715ccc2cff7426fd544761c9af258cadb49335d6
Katellyn Hyker <llyn00@yahoo.com>
2025-09-21 15:31:45 -0400

    Created templates file

---
+++ CarDealer/templates/templates.swift
@@ -0,0 +1,34 @@
+//
+// templates.swift
+// CarDealer
+//
+// Created by Katellyn Hyker on 9/21/25
+//
+
+import SwiftUI
+
+struct ThemedBackground<Content: View>: View {
+    let content: Content
+    @Environment(\.theme) var brand
+
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+
+    var body: some View{
+        ZStack {
+            RadialGradient(
+                gradient: Gradient(Colors: [brnad.palette.secondary,
+                    brand.palette.secondary,
+                    brand.palette.primary
+                ]),
+                center: .center,
+                startRadius: 60,
+                endRadius: 450
+            )
+            .ignoreSafeArea()
+
+            content
+        }
+    }
+}
